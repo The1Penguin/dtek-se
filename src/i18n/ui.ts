caa31633286cd1f56d5ccbb6d2a6e48ff61e5a28
pingu <nor@acorneroftheweb.com>
2024-12-02 18:25:38 +0100

    Translations?

---
+++ src/i18n/ui.ts
@@ -0,0 +1,45 @@
+export const languages = {
+  en: 'En',
+  sv: 'Sv',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'nav.name': 'Computer Science and Engineering',
+    'nav.union': 'Chalmers Student Union',
+    'nav.school': 'School',
+    'nav.dns': 'Committee of Studies',
+    'nav.schedule': 'Schedule',
+    'nav.rooms': 'Study spaces',
+    'nav.card': 'Student Union Card',
+    'nav.map': 'Map',
+    'nav.study': 'Support',
+    'nav.companies': 'Companies',
+    'nav.photo': 'Photos',
+    'nav.reception': 'Reception',
+    'nav.wiki': 'Wiki',
+    'nav.dbus': 'DBus',
+    'nav.documents': 'Documents',
+    'nav.newsletter': 'Newsletter'
+  },
+  sv: {
+    'nav.name': 'Datateknologsektionen',
+    'nav.union': 'Chalmers Studentkår',
+    'nav.school': 'Skola',
+    'nav.dns': 'Studienämnd',
+    'nav.schedule': 'Schema',
+    'nav.rooms': 'Grupprum',
+    'nav.card': 'Kårkort',
+    'nav.map': 'Karta',
+    'nav.study': 'Studiesocial hjälp',
+    'nav.companies': 'Företag',
+    'nav.photo': 'Foton',
+    'nav.reception': 'Mottagning',
+    'nav.wiki': 'Wiki',
+    'nav.dbus': 'DBus',
+    'nav.documents': 'Styrdokument',
+    'nav.newsletter': 'Veckobrev'
+  },
+} as const;
